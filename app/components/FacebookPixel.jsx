5dc5968be11ff27618ebfb47c3417ac0e405b0e3
Daniel Messori <messoridaniel@gmail.com>
2026-05-15 15:24:58 -0400

    fix 2 Facebook

--- app/components/FacebookPixel.jsx
+++ app/components/FacebookPixel.jsx
@@ -2,9 +2,6 @@
 
 import { useEffect, useRef } from "react"
 import { usePathname } from "next/navigation"
-import Script from "next/script"
-
-export const FB_PIXEL_ID = "1558086572418002"
 
 export default function FacebookPixel() {
   const pathname = usePathname()
@@ -20,20 +17,5 @@ export default function FacebookPixel() {
     }
   }, [pathname])
 
-  return (
-    <Script id="fb-pixel" strategy="afterInteractive">
-      {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '${FB_PIXEL_ID}');
-        fbq('track', 'PageView');
-      `}
-    </Script>
-  )
+  return null
 }
